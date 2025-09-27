8c0a30e9b2db12aa3f521e09ee60f41ba5a95c9c
PharaEthan <ethan1.hernou@epitech.eu>
2025-09-27 10:29:08 +0200

    build(Exporter.cs): Create an exporter scripts that export the needed files for building the packages

---
+++ anti-afk/Assets/Editor/Exporter.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class Exporter {
+
+    private static readonly string _ExportPath = "anti-afk/ExportedPackages/anti-afk.unitypackage";
+
+    [MenuItem("OMGG/Export Package")] // Allow Unity to call this method from the menu bar
+    public static void ExportPackage()
+    {
+        // All path to export from Assets folder
+        string[] assetsToExport = {
+            "Assets/OMGG/Package/anti-afk/Demo",
+            "Assets/OMGG/Package/anti-afk/Scripts",
+            "Assets/OMGG/Package/anti-afk/Resources",
+            "Assets/OMGG/Package/anti-afk/Editor"
+        };
+
+        // Create an export folder if needed
+        string dir = Path.GetDirectoryName(_ExportPath);
+
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+        // Export the package
+        AssetDatabase.ExportPackage(assetsToExport, _ExportPath, ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
+
+        Debug.Log($"Export completed: {_ExportPath}");
+    }
+}
